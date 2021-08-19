ca3bfa79242c2112a19fd4108a4cd78333facb5d
Lingrui <392425595@qq.com>
2021-08-19 17:50:06 +0800

    calculateBestPromotionAmount

---
+++ spec/promotion-spec.js
@@ -0,0 +1,83 @@
+const {discountPromotionCalculate} = require("../src/promotions");
+const {fullReductionPromotionCalculate} = require("../src/promotions");
+const {calculateBestPromotionAmount} = require("../src/promotions");
+
+describe('Test Promotion', function () {
+
+  it('should return best promotion amount when calculate given order items', function () {
+
+    let orderItems = [{
+      id: 'ITEM0001',
+      name: '黄焖鸡',
+      count: 1,
+      amount: 18.00
+    }, {
+      id: 'ITEM0013',
+      name: '肉夹馍',
+      count: 2,
+      amount: 12.00
+    }, {
+      id: 'ITEM0022',
+      name: '凉皮',
+      count: 1,
+      amount: 8.00
+    }]
+    const result = calculateBestPromotionAmount(orderItems)
+    let expected = {
+      order: orderItems,
+      bestPromotion: {
+        promotionType: {
+          type: '指定菜品半价',
+          items: ['ITEM0001', 'ITEM0022']
+        },
+        savedAmount: 13
+      },
+      amount: 25,
+    }
+    expect(result).toEqual(expected)
+  })
+
+  it('should return 6 amount when fullReductionPromotionCalculate given order items', function () {
+    let orderItems = [{
+      id: 'ITEM0001',
+      name: '黄焖鸡',
+      count: 1,
+      amount: 18.00
+    }, {
+      id: 'ITEM0013',
+      name: '肉夹馍',
+      count: 2,
+      amount: 12.00
+    }, {
+      id: 'ITEM0022',
+      name: '凉皮',
+      count: 1,
+      amount: 8.00
+    }]
+    const result = fullReductionPromotionCalculate(orderItems)
+    let expected = 6
+    expect(result.savedAmount).toEqual(expected)
+  })
+
+  it('should return 13 amount when discountPromotionCalculate given order items', function () {
+    let orderItems = [{
+      id: 'ITEM0001',
+      name: '黄焖鸡',
+      count: 1,
+      amount: 18.00
+    }, {
+      id: 'ITEM0013',
+      name: '肉夹馍',
+      count: 2,
+      amount: 12.00
+    }, {
+      id: 'ITEM0022',
+      name: '凉皮',
+      count: 1,
+      amount: 8.00
+    }]
+    const result = discountPromotionCalculate(orderItems)
+    let expected = 13
+    expect(result.savedAmount).toEqual(expected)
+  })
+});
--- src/promotions.js
+++ src/promotions.js
@@ -6,3 +6,44 @@ function loadPromotions() {
     items: ['ITEM0001', 'ITEM0022']
   }];
 }
+
+function fullReductionPromotionCalculate(orderItems) {
+  return {
+    savedAmount: getTotalAmount(orderItems) > 30 ? 6 : 0,
+    promotionType: {
+      type: '满30减6元'
+    }
+  }
+}
+
+function discountPromotionCalculate(orderItems) {
+  const promotionItems = ['ITEM0001', 'ITEM0022']
+  const needDiscountOrderItems = orderItems.filter(item => promotionItems.includes(item.id))
+  let savedAmount = needDiscountOrderItems.reduce((pre, cur) => pre + cur.amount, 0) * 0.5;
+
+  return {
+    savedAmount,
+    promotionType: {
+      type: '指定菜品半价',
+      items: ['ITEM0001', 'ITEM0022']
+    }
+  }
+}
+
+function getTotalAmount(orderItems) {
+  return orderItems.reduce((pre, cur) => pre + cur.amount, 0);
+}
+
+const calculateBestPromotionAmount = function (orderItems) {
+  const fullReductionPromotion = fullReductionPromotionCalculate(orderItems)
+  const discountPromotion = discountPromotionCalculate(orderItems)
+  const bestPromotion = fullReductionPromotion.savedAmount > discountPromotion.savedAmount ? fullReductionPromotion : discountPromotion
+
+  return {
+    order: orderItems,
+    bestPromotion,
+    amount: getTotalAmount(orderItems)-bestPromotion.savedAmount
+  }
+}
+
+module.exports = {calculateBestPromotionAmount, fullReductionPromotionCalculate, discountPromotionCalculate}
